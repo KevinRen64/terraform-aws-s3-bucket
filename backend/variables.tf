2b592857dfb1b18a3d6c230a77c641367e896320
Kevin Ren <renkevin64@gmail.com>
2026-01-12 22:15:15 +0000

    chore: enable remote backend with S3 and DynamoDB state locking

---
+++ backend/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+    type = string
+    default = "ap-southeast-2"
+}
+
+variable "backend_bucket_name" {
+    type = string
+    description = "Globally-unique S3 bucket name for Terraform state."
+}
+
+variable "lock_table_name" {
+    type = string
+    description = "DynamoDB table for state locking."
+    default = "terraform-locks"
+}
+
+variable "tags" {
+    type = map(string)
+    default = {}
+}
